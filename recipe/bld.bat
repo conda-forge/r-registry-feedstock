302b6a870351d0bf809455f59b17b65f1dfa6895
Isuru Fernando <isuruf@gmail.com>
2017-12-02 18:23:33 -0600

    Support building in windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,8 @@
 "%R%" CMD INSTALL --build .
 if errorlevel 1 exit 1
+
+set R_PACKAGE_NAME=%PKG_NAME:~2%
+mkdir %PREFIX%\bin
+echo ln -s ${PREFIX}/R/library/%R_PACKAGE_NAME% ${PREFIX}/lib/R/library/%R_PACKAGE_NAME% > %PREFIX%\bin\.%PKG_NAME%-post-link.sh
+echo rm ${PREFIX}/lib/R/library/%R_PACKAGE_NAME% > %PREFIX%\bin\.%PKG_NAME%-post-unlink.sh
+
